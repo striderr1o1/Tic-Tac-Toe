a5b2936a96c73321d205dfbc93e17509b27385d6
Ranger <mnipk1243@gmail.com>
2025-08-05 22:46:48 +0500

    add factory functions, getter setters

---
+++ script.js
@@ -0,0 +1,136 @@
+let gameboard =(function Grid(){
+    let rows = 3;
+    let columns = 3;
+    let grid = [];
+
+    for(let i = 0; i < rows; i++){
+        grid[i] = [];
+        for(let j = 0; j < columns;j++)
+        {
+            grid[i][j]="-";
+        }
+    }
+    const getRows = ()=>rows;
+    const getCol = ()=>columns;
+    const getGrid = ()=>grid;
+    const printGrid = ()=>{
+        for(let i = 0; i < rows; i++){
+            console.log(grid[i]);
+        }
+    }
+    const selectCell = (player)=>{
+        
+        let row = prompt("Enter row: ");
+        let col = prompt("Enter column:: ");
+
+        grid[row][col] = player.value;
+    }
+    return {getGrid, printGrid, selectCell};
+})();
+
+function playerFactory(){
+    let name;
+    let value = 0;
+    let win = false;
+
+    const setPlayerName = (name)=>{
+        name = name;
+    }
+    const getPlayerName = ()=>{
+        return name;
+    }
+
+    const setPlayerValue = (val)=>{
+        value = val;
+    }
+    const getPlayerValue = ()=>{
+        return value;
+    }
+
+    const setPlayerStatus = (w)=>{
+        win = w;
+    }
+    const getPlayerStatus = ()=>{
+        return win;
+    }
+    
+    
+    
+    return {getPlayerName, setPlayerName, getPlayerStatus, setPlayerStatus, setPlayerValue, getPlayerValue};
+    
+
+}
+
+
+
+function roundsFactory(player1, player2){
+    let round_no = 0;
+    let winner = null;
+    P1 = player1;
+    P2 = player2;
+    let count = 0;
+    let grid = gameboard.getGrid();
+
+    const turnP1 = ()=>{
+        gameboard.printGrid();
+        gameboard.selectCell(P1);
+    
+    }
+    const turnP2 = ()=>{
+        gameboard.printGrid();
+        gameboard.selectCell(P2);
+    
+    }
+    const checkWinStatus = (player)=>{
+        let value = player.value;
+        let rows = gameboard.getRows();
+        let columns = gameboard.getCol();
+        //win logic
+        
+        //check rows
+        let counter = 0;
+        let win = false;
+        for(let i = 0;i<rows;i++){
+            counter = 0;
+            for(let i = 0; i < columns;i++){
+                if(grid[i][j]===value){counter++;
+                    if (counter === 3){
+                    win = true;
+                    break;
+                }
+                }
+                
+            }
+        }
+    }
+    const roundFunctionality = ()=>{
+        do{if(count%2 === 0){
+            turnP2();
+            count++;
+        }
+        else{
+            turnP1();
+            count++;
+        }}while(winner === null)
+    }
+    return {roundFunctionality};
+    
+
+
+}
+
+(function Game(){
+    let P1 = playerFactory();
+    P1.setPlayerName("Mustafa");
+    P1.setPlayerValue("X");
+
+    let P2 = playerFactory();
+    P2.setPlayerName("Ali");
+    P2.setPlayerValue("O");
+
+//rounds
+})
+
+
+
+
